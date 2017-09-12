c0913bc00e56a09943beeefbbfba31c57f1ca10a
Daniel Pages <dspages@gmail.com>
2017-09-12 07:12:02 -0700

    Added a search feature to company index page

--- frontend/components/firm_index/firm_index.jsx
+++ frontend/components/firm_index/firm_index.jsx
@@ -6,10 +6,21 @@ class FirmList extends React.Component {
 
   constructor(props){
     super(props);
+    this.state = {searchVal: ""};
+    this.changeSearchVal = this.changeSearchVal.bind(this);
   }
 
   componentDidMount() {
     this.props.requestFirms();
+  }
+
+  changeSearchVal(event){
+    this.setState({searchVal: event.target.value});
+  }
+
+  searchFilter(firm){//Returns FALSE if item is excluded by search criteria
+    return firm.name.toLowerCase().indexOf(
+      this.state.searchVal.toLowerCase()) !== -1;
   }
 
   render() {
@@ -23,6 +34,7 @@ class FirmList extends React.Component {
     if(Object.values(firms.firmList).length > 0)
     {
       firmListItems = Object.values(firms.firmList);
+      firmListItems = firmListItems.filter(firm => this.searchFilter(firm));
       firmListItems = firmListItems.map(firm => (
           <FirmIndexItem
           name = {firm.name}
@@ -39,6 +51,10 @@ class FirmList extends React.Component {
     return(
       <div>
         <h1 className = "header">All Firms</h1>
+        <label>Search:
+          <input value={this.state.searchVal} onChange={this.changeSearchVal}>
+          </input>
+        </label>
         <div className = "main-box">
           <ul className = "firm-list">
             {firmListItems}
